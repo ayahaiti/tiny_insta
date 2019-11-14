29e3a0e0e750de15d1f404bca72820d9e27bfd5d
haiti <haitiaya@gmail.com>
2019-11-14 17:00:26 +0100

    testing shell starter

---
+++ src/main/java/io/tiny/insta/tinyinstagram/repositories/BookRepository.java
@@ -0,0 +1,16 @@
+package io.tiny.insta.tinyinstagram.repositories;
+
+import org.springframework.cloud.gcp.data.datastore.repository.DatastoreRepository;
+
+import java.util.List;
+
+
+public interface BookRepository extends DatastoreRepository<Book, Long> {
+
+  List<Book> findByAuthor(String author);
+
+  List<Book> findByYearGreaterThan(int year);
+
+  List<Book> findByAuthorAndYear(String author, int year);
+
+}
