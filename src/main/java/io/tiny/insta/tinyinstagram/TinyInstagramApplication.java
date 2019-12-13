b71fa33ededbb76c4aa1c53db3132306a9e89f3c
haiti <haitiaya@gmail.com>
2019-12-14 00:23:09 +0100

    todo update

--- src/main/java/io/tiny/insta/tinyinstagram/TinyInstagramApplication.java
+++ src/main/java/io/tiny/insta/tinyinstagram/TinyInstagramApplication.java
@@ -6,11 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TinyInstagramApplication {
 
+    // set button instead of love icon => do like follow button
+
+    // reload feed every 5seconds => load only 6 last images
+
+    // TODO lock all button after click + show error messages
+
+    // show error messages like feed : calendar904
+
     // TODO add exceptions : usernameAlreadyExists for user-create
     // TODO add exceptions : usernameAndPasswordKO for user-connect + username&password ko
     // TODO add exceptions : usernameAndTokenKO for the rest of ws
-
-    // TODO lock all button after click + show error messages
 
     // TODO add auto-scaling config gcp https://www.baeldung.com/spring-boot-google-app-engine
 
