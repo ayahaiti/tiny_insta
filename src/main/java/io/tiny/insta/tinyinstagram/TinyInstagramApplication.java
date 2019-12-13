d4bb36c0f9a1efa2b10bf362ef4d7a72c7d4e499
haiti <haitiaya@gmail.com>
2019-12-14 00:28:41 +0100

    update TODOs

--- src/main/java/io/tiny/insta/tinyinstagram/TinyInstagramApplication.java
+++ src/main/java/io/tiny/insta/tinyinstagram/TinyInstagramApplication.java
@@ -6,13 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TinyInstagramApplication {
 
-    // set button instead of love icon => do like follow button
+    // TODO set button instead of love icon => do like follow button
 
-    // reload feed every 5seconds => load only 6 last images
+    // TODO lock all button after click until end WS call
+    // TODO show notifications https://www.npmjs.com/package/angular-notifier
 
-    // TODO lock all button after click + show error messages
-
-    // show error messages like feed : calendar904
+    // TODO reload feed every 5seconds => load only 6 last images
 
     // TODO add exceptions : usernameAlreadyExists for user-create
     // TODO add exceptions : usernameAndPasswordKO for user-connect + username&password ko
