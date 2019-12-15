a901a153932fd9b7cb854b1cec684071000d4676
haiti <haitiaya@gmail.com>
2019-12-15 18:17:08 +0100

    fix message de chargement

--- src/main/java/io/tiny/insta/tinyinstagram/TinyInstagramApplication.java
+++ src/main/java/io/tiny/insta/tinyinstagram/TinyInstagramApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TinyInstagramApplication {
 
-    // 15min
+    // 30min
+    // TODO traduire en français
+    // TODO fix image preview => make it bigger
     // TODO remove all posts and add pretty ones
 
-    // 45min
+    // 30min
     // TODO add auto-scaling config gcp https://www.baeldung.com/spring-boot-google-app-engine
 
     public static void main(String[] args) {
